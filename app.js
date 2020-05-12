ccded25802d889562e0b6af8807e47b775bc0bf5
rechielagria <rechielagria@gmail.com>
2020-05-13 07:13:45 +0800

    initial commuit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const postRouter = require('./routes/posts');
+const mongoose = require('mongoose');
+const app = express();
+
+//Body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/posts', postRouter);
+
+app.get('/', (req, res) => {
+	res.send('We are on home');
+});
+
+mongoose.connect(
+	'mongodb://rechie:iloveRuthy123@cluster0-shard-00-00-f4fzr.mongodb.net:27017,cluster0-shard-00-01-f4fzr.mongodb.net:27017,cluster0-shard-00-02-f4fzr.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+	{ useNewUrlParser: true, useUnifiedTopology: true },
+	() => {
+		console.log('Connected to DB');
+	}
+);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server is running at port ${PORT}`));
---
+++ routes/posts.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../model/Post');
+
+router.get('/', (req, res) => {
+	res.send('We are on posts');
+});
+
+router.post('/', async (req, res) => {
+	const post = new Post({
+		title: req.body.title,
+		description: req.body.description
+	});
+	try {
+		const savedPost = await post.save();
+		res.json(savedPost);
+	} catch (err) {
+		res.json({ err: err });
+	}
+});
+
+module.exports = router;
